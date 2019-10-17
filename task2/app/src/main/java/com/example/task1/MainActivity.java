4b8dbf31a6348f54215a6e23fe6d8f57d0d3f9e8
Zheltok Mariya <masha.zheltok@gmail.com>
2019-10-17 18:52:14 +0300

    add lab2

---
+++ task2/app/src/main/java/com/example/task1/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.task1;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static  final int REQUEST_ANSWER=10;
+    static final String QUESTION_KEY = "question";
+
+    private EditText askQuestion;
+    private Button buttonQuestion;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        askQuestion = (EditText)findViewById(R.id.askQuestion);
+        buttonQuestion = (Button)findViewById(R.id.buttonQuestion);
+
+        buttonQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AnswerTheQuestionActivity.class);
+                intent.putExtra(QUESTION_KEY, askQuestion.getText().toString());
+                startActivityForResult(intent, REQUEST_ANSWER);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == REQUEST_ANSWER) {
+            if (resultCode == RESULT_OK) {
+                String textOfAnswer = data.getStringExtra("answer");
+                TextView answer = (TextView)findViewById(R.id.answer);
+                answer.setText(textOfAnswer);
+            }
+        }
+    }
+
+    public void onConfigurationChanged(Configuration newConfig){
+        super.onConfigurationChanged(newConfig);
+    }
+
+}
